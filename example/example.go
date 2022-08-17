f61c2e006166125c1e2a3b5ed6edc63204f6465d
Sam <sam@getblock.io>
2022-08-17 05:59:55 +0300

    add example

---
+++ example/example.go
@@ -0,0 +1,52 @@
+package example
+
+import (
+	"runtime"
+	zstdM "zstd-marshal"
+)
+
+type SomeStruct struct {
+	Field       string `json:"Field"`
+	SecondField string `json:"SecondField"`
+}
+
+func EncodeAndDecode() {
+
+	someData := SomeStruct{
+		"Test",
+		"Test2",
+	}
+
+	encodedDataBytes, err := zstdM.Marshal(someData)
+
+	if err != nil {
+		return
+	}
+
+	var stForDecode SomeStruct
+
+	err = zstdM.Unmarshal(encodedDataBytes, &stForDecode)
+	if err != nil {
+		return
+	}
+}
+
+func EncodeAndDecodeWithConcurrency() {
+	someData := SomeStruct{
+		"Test",
+		"Test2",
+	}
+
+	encodedDataBytes, err := zstdM.MarshalWithConcurrency(someData, runtime.GOMAXPROCS(-1))
+
+	if err != nil {
+		return
+	}
+
+	var stForDecode SomeStruct
+
+	err = zstdM.UnmarshalWithConcurrency(encodedDataBytes, &stForDecode, runtime.GOMAXPROCS(-1))
+	if err != nil {
+		return
+	}
+}
